db351d13009e88332e2c66fffda6204a965c7f26
lmackay <lmackay@protic.(none)>
2013-04-12 12:37:24 -0400

    derivative script

--- numDer.pl
+++ numDer.pl
@@ -6,8 +6,9 @@ use Cwd;
 my $file_name = $ARGV[0];
 my $cwd = getcwd();
 my $seperator=0;
-my $dx=$ARGV[n];
 my $bohr=0.529;
+my $dx=$ARGV[0];
+
 
 my $status=system("g09 ref.com");
 my $energy;
@@ -19,18 +20,19 @@ my $energy;
 my $factor=1; 
 my $delta=1;
 my $coord;
+my $factor=27.211;
 
 open(INPUT,"<$cwd/ref.log") or die "Cannot open $cwd/ref.log to read: $!\n";
 while (my $line = <INPUT>) {
 #print $line;
 	if($line =~ /E\(\w+\)\s+=\s+(\S+)/){
 		$refEnergy=$1;
-		#print "$refEnergy\n";
+		print "E0=$refEnergy\n";
 	} 
 	if($delta && $line =~ /(\d+)( +)(\d+)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)/){
-				$forces="$5$6"*$factor."   "."$8$9"*$factor."   "."$11$12"*$factor;				
+				$forces="$5$6"."   "."$8$9"."   "."$11$12";				
 				$refForces=$forces;
-				print  "Gaussian force (Hartree/bohr): $refForces\n";
+				#print  "Gaussian force (Hartree/bohr): $refForces\n";
 				$delta=0;
 			}
 }
@@ -42,8 +44,8 @@ open(INPUT,"<$cwd/ref.com") or die "Cannot open $cwd/ref.com to read: $!\n";
 open(OUTPUT,">$cwd/tempx.com")  or die "Cannot open $cwd/temxp.com to read: $!\n";
 while (my $line = <INPUT>) {
 	if($delta && $line =~/( +)(\w+)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)/){
-		$coord="$4$5"+$dx*$bohr;
-    #print "$4$5 -> $coord\n";
+		$coord="$4$5"+($dx*$bohr);
+    print "$4$5 -> $coord\n";
 		print OUTPUT "$1$2$3"."$coord"."$6$7$8$9$10$11\n";
 		$delta=0;
 	}else{
@@ -59,8 +61,9 @@ open(INPUT,"<$cwd/tempx.log") or die "Cannot open $cwd/ref.log to read: $!\n";
 while (my $line = <INPUT>) {
 	if($line =~ /E\(\w+\)\s+=\s+(\S+)/){
 		$energy=$1;
+		print "Ex=$energy\n";
 	}
-	$force=($energy-$refEnergy)/$dx;#Fx
+	$force=$factor*($energy-$refEnergy)/$dx;#Fx
 }
 close(INPUT);
 
@@ -72,7 +75,7 @@ open(INPUT,"<$cwd/ref.com") or die "Cannot open $cwd/ref.com to read: $!\n";
 open(OUTPUT,">$cwd/tempy.com")  or die "Cannot open $cwd/temp.com to read: $!\n";
 while (my $line = <INPUT>) {
 	if($delta && $line =~/( +)(\w+)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)/){
-		$coord="$7$8"+$dx*$bohr;
+		$coord="$7$8"+($dx*$bohr);
 		#print "$7$8 -> $coord\n";
 		print OUTPUT "$1$2$3$4$5$6"."$coord"."$9$10$11\n";
 		$delta=0;
@@ -89,8 +92,9 @@ open(INPUT,"<$cwd/tempy.log") or die "Cannot open $cwd/ref.log to read: $!\n";
 while (my $line = <INPUT>) {
 	if($line =~ /E\(\w+\)\s+=\s+(\S+)/){
 		$energy=$1;
+		print "Ey=$energy\n";
 	}
-	$force=($energy-$refEnergy)/$dx;#Fx
+	$force=$factor*($energy-$refEnergy)/$dx;#Fx
 }
 close(INPUT);
 
@@ -102,7 +106,7 @@ open(INPUT,"<$cwd/ref.com") or die "Cannot open $cwd/ref.com to read: $!\n";
 open(OUTPUT,">$cwd/tempz.com")  or die "Cannot open $cwd/temp.com to read: $!\n";
 while (my $line = <INPUT>) {
 	if($delta && $line =~/( +)(\w+)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)/){
-		$coord="$10$11"+$dx*$bohr;
+		$coord="$10$11"+($dx*$bohr);
 		#print "$10$11 -> $coord\n";
 		print OUTPUT "$1$2$3$4$5$6$7$8$9"."$coord\n";
 		$delta=0;
@@ -119,13 +123,14 @@ open(INPUT,"<$cwd/tempz.log") or die "Cannot open $cwd/ref.log to read: $!\n";
 while (my $line = <INPUT>) {
 	if($line =~ /E\(\w+\)\s+=\s+(\S+)/){
 		$energy=$1;
+		print "Ez=$energy\n";
 	}
-	$force=($energy-$refEnergy)/$dx;#Fx
+	$force=$factor*($energy-$refEnergy)/$dx;#Fx
 }
 close(INPUT);
 
 $forces="$forces   $force";
-
+print  "Gaussian force (Hartree/bohr): $refForces\n";
 print "Numerical Force (Hartree/bohr): $forces\n"
 
 
