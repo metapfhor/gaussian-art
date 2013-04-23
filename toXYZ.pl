51604f7840eeb43bff70cc5fd40db1d848308ee4
lmackay <lmackay@CSFG2UA1170832.(none)>
2013-04-23 14:53:19 -0400

    toXYZ.pl now has regex for multiple file merging

--- toXYZ.pl
+++ toXYZ.pl
@@ -1,23 +1,40 @@
 #!/usr/local/bin/perl
 #!/usr/local/bin/perl
+use strict;
+use File::Find;
 use Cwd;
 
-my $file_name = $ARGV[0];
+my $regex = $ARGV[0];
 my $cwd = getcwd();
 my $coords="";
 my $line_counter;
 my @elements=("","H","He","Li","Be","B","C","N","O","F","Ne");
 
-open(INPUT,"<$cwd/$file_name") or die "Cannot open $cwd/$file_name to read: $!\n";
-open(OUTPUT,">$cwd/$file_name.xyz")  or die "Cannot open $cwd/$file_name.xyz to read: $!\n";
-while (my $line = <INPUT>){
-    if($line =~ /(\d+)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)/){
-      $coords=$coords."$elements[$1]   $3   $6   $9 \n";
-      $line_counter++;
+open(OUTPUT,">$cwd/$regex.xyz")  or die "Cannot open $cwd/$regex.xyz to read: $!\n";
+opendir(DIR, $cwd);
+my @FILES= readdir(DIR);
+closedir(DIR);
+
+foreach(@FILES){
+  if($_ =~ /$regex/ && $_!~/\.xyz/){
+    $coords="";
+    $line_counter=0;
+    open(INPUT,"<$cwd/$_") or die "Cannot open $cwd/$$_ to read: $!\n";
+    while (my $line = <INPUT>){
+      if($line =~ /(\d+)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)( +)(-?\d+\.\d+([Ee][+-]?\d+)?)/){
+       $coords=$coords."$elements[$1]   $3   $6   $9 \n";
+       $line_counter++;
+      }
     }
+   print OUTPUT "$line_counter\n";
+   print OUTPUT "$_\n";
+   print OUTPUT "$coords";
+   print  "$line_counter\n";
+   print  "$_\n";
+   print  "$coords";
+   close(INPUT);
+  }
 }
-print OUTPUT "$line_counter\n";
-print OUTPUT "$file_name\n";
-print OUTPUT "$coords";
+
 close(OUTPUT);
-close(INPUT);
+
